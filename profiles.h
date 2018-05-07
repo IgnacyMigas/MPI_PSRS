39de03f4794feddcada3cc9cd5958f092aab60e8
ignacy <gigi.migas@gmail.com>
2018-05-08 01:46:51 +0200

    Add profile to Gather and Scatter

--- profiles.h
+++ profiles.h
@@ -16,6 +16,12 @@
 #define END_SEND 7
 #define START_SCAT 8
 #define END_SCAT 9
+#define START_SCATV 10
+#define END_SCATV 11
+#define START_GATH 12
+#define END_GATH 13
+#define START_GATHV 14
+#define END_GATHV 15
 
 int MPI_Init(int *argc, char ***argv);
 
@@ -29,21 +35,24 @@ int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, M
 
 int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
 
-/*
-int MPI_Scatterv(const void *sendbuf, int *sendcnts, int *displs, MPI_Datatype sendtype, 
-                    void *recvbuf, int recvcnt, MPI_Datatype recvtype, int root, MPI_Comm comm)
-{ 
-    int ret, myid;
+int MPI_Scatter(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, 
+                    MPI_Datatype recvtype, int root, MPI_Comm comm);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+int MPI_Scatterv(const void *sendbuf, const int *sendcounts, const int *displs, MPI_Datatype sendtype, 
+                    void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
 
-	MPE_Log_event(START_SCAT, myid, "Start Scatterv");
-   
-    ret = PMPI_Scatterv(sendbuf, sendcnts, displs, sendtype, recvbuf, recvcnt, recvtype, root, comm);
+int MPI_Scatter(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, 
+                    MPI_Datatype recvtype, int root, MPI_Comm comm);
 
-	MPE_Log_event(END_SCAT, myid, "End Scatterv");
-   
-    return ret; 
-} */
+int MPI_Scatterv(const void *sendbuf, const int *sendcounts, const int *displs, MPI_Datatype sendtype, 
+                    void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
+
+int MPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf,
+               int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
+
+int MPI_Gatherv(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf,
+                const int *recvcounts, const int *displs, MPI_Datatype recvtype, int root,
+                MPI_Comm comm);
+
 
 #endif
