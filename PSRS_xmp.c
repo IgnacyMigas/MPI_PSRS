f9dc044eb1a71d569974289144aa4967dc8c6a73
Lazu13 <tomek.laz.13@gmail.com>
2018-06-11 10:00:33 +0200

    test

--- PSRS_xmp.c
+++ PSRS_xmp.c
@@ -16,9 +16,13 @@ int main(int argc, char *argv[]) {
     int i, j, ret;
     double startwtime = 0.0, endwtime;
 
+    numprocs = xmp_num_nodes();
+    myid = xmp_node_num();
     server = 0;
 
-    #pragma xmp task on p[0]
+    printf("My id is %d\n", myid);
+
+    #pragma xmp task on p[server]
     {
         printf("\nStarting\n");
         printf("Number of processes %d\n", numprocs);
